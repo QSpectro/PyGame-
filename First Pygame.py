50a5b94bb4e9042cb73f02ed2e0f94fa94d6fdc5
QSpectro <139924919+QSpectro@users.noreply.github.com>
2024-06-27 15:08:08 -0500

    Add files via upload

---
+++ First Pygame.py
@@ -0,0 +1,14 @@
+#Intoduction to the game
+print ('Welcome to LycanSquad!')
+print ('This is my first game so please if you can help or donate, I\'ll leave a link below where you can contribute.')
+print ('Again, Thank You and I hope you enjoy the game.')
+
+#Create Character and Choose Stats
+class Character:
+    def __init__(self, name, build_type):
+
+
+
+#Starts the Game and tells the player how to play 
+#Player leveling
+#Gameplay
